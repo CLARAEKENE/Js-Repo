0bb25b0cc0d636bd3e65105b3eb6a3205c547f15
CLARAEKENE <claraekene39@gmail.com>
2022-07-08 04:59:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+console.log('Hello Mentors');
+
+
+
+// question1:
+let age = 70
+console.log(age);
+
+const height = 70
+console.log(height);
+
+var firstName = 'clara'
+console.log(firstName);
+
+
+
+// question2:
+let schoolName = 'unical'
+console.log(schoolName);
+
+let example = 20
+console.log(example);
+
+let example2 = false
+console.log(example2);
+
+let studentsAges = [14, 15, 16]
+console.log(studentsAges);
+
+let example4 = {
+    bookName:'textbook',
+    computerName: 'lenovo'
+}
+console.log(example4);
+console.log(example4.computerName);
+
+let officeName = undefined
+console.log(officeName);
+
+let selectedColor = null
+console.log(selectedColor);
+
+
+
+// question3:
+let example20 = 100 +100
+console.log(example20);
+
+let example30 = 50-5
+console.log(example30);
+
+let example40 = 150*2
+console.log(example40);
+
+let example50 = 16/2
+console.log(example50);
+
+
+
+// question4:
+let x = 10;
+x += 5;
+console.log(x);
+
+
+
+// question5: 
+A GLOBAL SCOPE is a variable that is been declared in the root 
+of the file and can be accessed from anywhere in the context of a browser. 
+It is declared outside the main block and is available throughOut the program.
+
+A LOCAL SCOPE is variable that is been declared inside the main block and is available within the block only.    
+
+
+
+
+
+
+
+
+
+
